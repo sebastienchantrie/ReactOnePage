d544a7a83c48836929db2fa60a855feb8ba1269b
Sébastien Chantrie <sebastien.chantrie@gmail.com>
2019-09-09 11:47:30 +0200

    ajoujou

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -9,8 +9,8 @@ class Jumbo extends Component {
 
                 <div className="col-12 bgimg">
                 <span className="icon solid major fas fa-4x fa-cloud mb-4"></span>
-                    <h1 className='mb-4'>Hi I'm photon, another fine,<br/>
-                    little freebie from HTML5 UP.
+                    <h1 className='mb-4'>Projet reproduire a l'identique<br/>
+                    un template HTML5 !
                     </h1>
                     <p className='mb-4'>Accumsan feugiat mi commodo erat lorem ipsum, sed magna <br/>
 lobortis feugiat sapien sed etiam volutpat accumsan.</p>
